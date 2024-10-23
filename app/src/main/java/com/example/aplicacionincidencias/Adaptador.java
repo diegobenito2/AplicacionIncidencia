402644986270ffc00eadacf8fbff5e0150bdf6dd
Diego <diegobenitomartinez02@gmail.com>
2024-10-23 16:07:03 +0200

    Semicompletado el Adaptador falta solucionar los errores.

--- app/src/main/java/com/example/aplicacionincidencias/Adaptador.java
+++ app/src/main/java/com/example/aplicacionincidencias/Adaptador.java
@@ -1,7 +1,11 @@
 package com.example.aplicacionincidencias;
 import com.example.aplicacionincidencias.activitySalas;
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
@@ -12,6 +16,21 @@ public class Adaptador extends ArrayAdapter<Sala> {
     public Adaptador(Context c, Sala[] salas){
         super(c,R.layout.elemento_sala,salas);
         this.datos=salas;
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        View vSala = convertView;
+        if (vSala ==null){
+            LayoutInflater mostrado = LayoutInflater.from(getContext());
+            View VSala = mostrado.inflate(R.layout.elemento_sala,parent,false);
+        }
+
+        TextView txCodigo = vSala.findViewById(R.id.codigoSala);
+        TextView txNombre = vSala.findViewById(R.id.nombreSala);
+
+        txCodigo.setText(String.valueOf(datos[positon].getCodigoSala()));
+        txNombre.setText(datos[position].getNombre());
+        return VSala;
     }
 
 }
