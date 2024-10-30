006f7bbe53ed9efc38cd326d734ad73c38246a2c
Diego <diegobenitomartinez02@gmail.com>
2024-10-30 14:26:05 +0100

    Update Incidencias

--- app/src/main/java/com/example/aplicacionincidencias/Incidencia/AdaptadorIncidencia.java
+++ app/src/main/java/com/example/aplicacionincidencias/Incidencia/AdaptadorIncidencia.java
@@ -1,27 +1,39 @@
 package com.example.aplicacionincidencias.Incidencia;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+
 import com.example.aplicacionincidencias.R;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 import gestionincidencias.entidades.Incidencia;
 
 public class AdaptadorIncidencia extends ArrayAdapter<Incidencia> {
     private Incidencia[] datos;
 
+
     public AdaptadorIncidencia(Context c, Incidencia[] incidencias) {
         super(c, R.layout.elemento_incidencia, incidencias);
         this.datos = incidencias;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
+        Date fechaActual = new Date();
+        SimpleDateFormat formato = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+        String fechaFormateada = formato.format(fechaActual);
         LayoutInflater mostrado = LayoutInflater.from(getContext());
         View VIncidencia = mostrado.inflate(R.layout.elemento_incidencia, parent, false);
 
@@ -30,12 +42,14 @@ public class AdaptadorIncidencia extends ArrayAdapter<Incidencia> {
         TextView txIdElemento = VIncidencia.findViewById(R.id.idElemento);
         TextView txIdUsuarioCreacion = VIncidencia.findViewById(R.id.IdUsuarioCreacion);
         TextView txIdResponsable = VIncidencia.findViewById(R.id.IdResponsable);
+        TextView txFechaCreacion = VIncidencia.findViewById(R.id.FechaCreacion);
 
-        txCodigo.setText(String.valueOf(datos[position].getCodigoIncidencia()));
-        txDescripcion.setText(datos[position].getDescripcion());
-        txIdElemento.setText(String.valueOf(datos[position].getIdElemento()));
-        txIdUsuarioCreacion.setText(String.valueOf(datos[position].getIdUsuarioCreacion()));
-        txIdResponsable.setText(String.valueOf(datos[position].getIdResponsable()));
+        txCodigo.setText("Código Incidencia: " + String.valueOf(datos[position].getCodigoIncidencia()));
+        txDescripcion.setText("Descripción: " + datos[position].getDescripcion());
+        txIdElemento.setText("Id Elemento: " + String.valueOf(datos[position].getIdElemento()));
+        txIdUsuarioCreacion.setText("Id Usuario Creación: " + String.valueOf(datos[position].getIdUsuarioCreacion()));
+        txIdResponsable.setText("Id Responsable: " + String.valueOf(datos[position].getIdResponsable()));
+        txFechaCreacion.setText("Fecha Creación: " + String.valueOf(fechaFormateada));
         return VIncidencia;
     }
 }
