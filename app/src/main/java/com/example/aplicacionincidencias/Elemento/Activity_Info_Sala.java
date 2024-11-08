54f998852496fbc6323941749ee4307d56e43111
Diego <diegobenitomartinez02@gmail.com>
2024-11-08 10:12:43 +0100

    Activity_Info_Sala.xml hecho

---
+++ app/src/main/java/com/example/aplicacionincidencias/Elemento/Activity_Info_Sala.java
@@ -0,0 +1,27 @@
+package com.example.aplicacionincidencias.Elemento;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.aplicacionincidencias.R;
+
+public class Activity_Info_Sala extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_info_sala);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
