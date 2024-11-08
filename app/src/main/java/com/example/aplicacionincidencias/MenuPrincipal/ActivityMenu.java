9470323d5161770b0fe0dc6ef78a2824581b16cb
Diego <diegobenitomartinez02@gmail.com>
2024-11-08 09:26:33 +0100

    Menu Inicial

--- app/src/main/java/com/example/aplicacionincidencias/MenuPrincipal/ActivityMenu.java
+++ app/src/main/java/com/example/aplicacionincidencias/MenuPrincipal/ActivityMenu.java
@@ -1,6 +1,9 @@
 package com.example.aplicacionincidencias.MenuPrincipal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.aplicacionincidencias.Elemento.activityElemento;
+import com.example.aplicacionincidencias.Incidencia.activityIncidencia;
+import com.example.aplicacionincidencias.Prestamo.ActivityPrestamo;
 import com.example.aplicacionincidencias.R;
+import com.example.aplicacionincidencias.Sala.activitySalas;
+import com.example.aplicacionincidencias.Tipo.activityTipo;
+import com.example.aplicacionincidencias.Ubicacion.activityUbicacion;
+import com.example.aplicacionincidencias.Usuario.ActivityUsuario;
 
-public class ActivityMenu extends AppCompatActivity {
+public class ActivityMenu extends AppCompatActivity implements View.OnClickListener {
+    private Button ButtonMenuSala;
+    private Button ButtonMenuTipo;
+    private Button ButtonMenuPrestamo;
+    private Button ButtonMenuUbicacion;
+    private Button ButtonMenuUsuario;
+    private Button ButtonMenuElemento;
+    private Button ButtonMenuIncidencia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +39,61 @@ public class ActivityMenu extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        initComponents();
+        initListeners();
+    }
+
+    private void initComponents() {
+        ButtonMenuSala = findViewById(R.id.ButtonMenuSala);
+        ButtonMenuTipo = findViewById(R.id.ButtonMenuTipo);
+        ButtonMenuPrestamo = findViewById(R.id.ButtonMenuPrestamo);
+        ButtonMenuUbicacion = findViewById(R.id.ButtonMenuUbicacion);
+        ButtonMenuUsuario = findViewById(R.id.ButtonMenuUsuario);
+        ButtonMenuElemento = findViewById(R.id.ButtonMenuElemento);
+        ButtonMenuIncidencia = findViewById(R.id.ButtonMenuIncidencia);
+
+    }
+
+    public void initListeners() {
+        ButtonMenuSala.setOnClickListener((View.OnClickListener) this);
+        ButtonMenuTipo.setOnClickListener((View.OnClickListener) this);
+        ButtonMenuPrestamo.setOnClickListener((View.OnClickListener) this);
+        ButtonMenuUbicacion.setOnClickListener((View.OnClickListener) this);
+        ButtonMenuUsuario.setOnClickListener((View.OnClickListener) this);
+        ButtonMenuElemento.setOnClickListener((View.OnClickListener) this);
+        ButtonMenuIncidencia.setOnClickListener((View.OnClickListener) this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        ButtonMenuSala.setOnClickListener(v -> {
+            Intent intent = new Intent(this, activitySalas.class);
+            startActivity(intent);
+        });
+        ButtonMenuTipo.setOnClickListener(v -> {
+            Intent intent = new Intent(this, activityTipo.class);
+            startActivity(intent);
+        });
+        ButtonMenuPrestamo.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ActivityPrestamo.class);
+            startActivity(intent);
+        });
+        ButtonMenuUbicacion.setOnClickListener(v -> {
+            Intent intent = new Intent(this, activityUbicacion.class);
+            startActivity(intent);
+        });
+        ButtonMenuUsuario.setOnClickListener(v -> {
+            Intent intent = new Intent(this, ActivityUsuario.class);
+            startActivity(intent);
+        });
+        ButtonMenuElemento.setOnClickListener(v -> {
+            Intent intent = new Intent(this, activityElemento.class);
+            startActivity(intent);
+        });
+        ButtonMenuIncidencia.setOnClickListener(v -> {
+            Intent intent = new Intent(this, activityIncidencia.class);
+            startActivity(intent);
+        });
+
     }
 }
